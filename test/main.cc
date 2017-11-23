f794e144e4a1b20c0ffdee4f57a6432c9324cb8f
Eric Martin <ericmartin827@gmail.com>
2017-11-23 11:36:25 -0800

    Slight styles changes to main.cc

--- test/main.cc
+++ test/main.cc
@@ -7,17 +7,19 @@
 
 #define PATH_MAX 200
 
-int main(int argc, char **argv) {
+int
+main(int argc, char **argv)
+{
 
 	char* cwd;
-    char buff[PATH_MAX + 1];
-    cwd = getcwd( buff, PATH_MAX + 1 );
-    if( cwd == NULL ) {
-        std::cerr << "Failed to retrieve the current directory." << std::endl;
-        return EXIT_FAILURE;
-    }
+	char buff[PATH_MAX + 1];
+	cwd = getcwd( buff, PATH_MAX + 1 );
+	if( cwd == NULL ) {
+		std::cerr << "Failed to retrieve the current directory." << std::endl;
+		return EXIT_FAILURE;
+	}
+
 	const std::string filename = std::string(cwd) + "/config.ini";
-	
 	MapReduce job;
 	return job.run(filename) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
